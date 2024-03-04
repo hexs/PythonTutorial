f3ee6c2e32437734ce8ccfef22b47964038d51ae
jj <zxjq97@gmail.com>
2024-03-04 17:05:00 +0700

    modify image_to_text/main.py and image_to_text/read_barcode.py

--- image_to_text/main.py
+++ image_to_text/main.py
@@ -1,5 +1,18 @@
 import time
 import cv2
+from datetime import datetime
+
+BLACK = '\033[90m'
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+PINK = '\033[95m'
+CYAN = '\033[96m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+ITALICIZED = '\033[3m'
+UNDERLINE = '\033[4m'
 
 
 def putTextRect(img, text, pos, font, scale, colorT=(255, 255, 255), thickness=3,
@@ -18,13 +31,16 @@ def capture(data):
     cap = cv2.VideoCapture(0)
 
     while True:
-        data['cap'] = cap.read()
+        s, img = cap.read()
+        if s:
+            data['cap'] = s, cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        else:
+            time.sleep(1)
+            cap = cv2.VideoCapture(0)
 
 
 def main(data):
-    import cv2
     import pytesseract
-    from datetime import datetime
     from read_barcode import read_barcodes
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     t1 = datetime.now()
@@ -46,22 +62,25 @@ def main(data):
                     data['MC'] = text
                 if 'QTY' in text:
                     data['QTY'] = text
-                if 'LN' in text:
+                if 'LN' in text or 'N:' in text:
+                    print(BLUE, text, ENDC)
+                    text = text.split('N')[-1]
                     ndmy = text.strip().strip('LN').strip().strip(':').strip()
+                    print(PINK, ndmy, ENDC)
                     if len(ndmy) == 7:
                         l = ndmy[0:2]
-                        d = ndmy[2:4]
+                        y = ndmy[2:4]
                         m = ndmy[4]
-                        y = ndmy[5:]
+                        d = ndmy[5:]
                         print((l, d, m, y,))
-                        if all(i.isdigit() for i in (l, d, y)) and m in 'ABCDEFGHIJKL':
+                        if all(i.isdigit() for i in (d, y)) and m in 'ABCDEFGHIJKL':
                             d = int(d)
                             y = int(y) + 2000
                             m = ord(m) - ord('A') + 1
                             if 1 <= d <= 31:
-                                data['LN'] = text
+                                data['LN'] = ndmy
                                 data['date'] = datetime(year=y, month=m, day=d)
-                                data['lot'] = int(l)
+                                data['lot'] = l
 
                                 data['data complete'] = True, data['data complete'][1]
 
@@ -69,11 +88,10 @@ def main(data):
             if bar:
                 data['barcode'] = bar
                 if len(bar) == 20 and '92' in bar and '30' in bar:
-                    MC, QTY = bar.split('30')
-                    MC = bar.split(QTY)[0]
-
+                    bar = bar.replace('92', '[').replace('30', ']')
+                    QTY = bar.split(']')[1]
                     data['bar QTY'] = QTY
-                    data['bar MC'] = MC
+                    data['bar MC'] = bar[2:-2].replace('[', '').replace(']', '')
 
                     data['data complete'] = data['data complete'][0], True
 
@@ -83,43 +101,36 @@ def getkey(data):
     import keyboard
 
     while True:
-        event = keyboard.read_event()
-        stamp_time = event.time
-        event_type = event.event_type
-        name = event.name
-        if event_type == 'up' and name == 'right ctrl':
-            if all(data['data complete']):
-                pyperclip.copy(data['bar MC'])
-                keyboard.press_and_release('Ctrl + v, \n')
-                time.sleep(0.1)
-                pyperclip.copy(data['bar QTY'])
-                time.sleep(0.1)
-                keyboard.press_and_release('Ctrl + v, \n')
-                time.sleep(0.1)
-                pyperclip.copy(data['LN'])
-                time.sleep(0.1)
-                keyboard.press_and_release('Ctrl + v, \n')
-                time.sleep(0.1)
-                pyperclip.copy(str(data['lot']))
-                time.sleep(0.1)
-                keyboard.press_and_release('Ctrl + v, \n')
-                time.sleep(0.1)
-                pyperclip.copy(str(data['date']))
-                time.sleep(0.1)
-                keyboard.press_and_release('Ctrl + v, \n')
+        # event = keyboard.read_event()
+        # stamp_time = event.time
+        # event_type = event.event_type
+        # name = event.name
+        # if event_type == 'up' and name == 'right ctrl':
+        if all(data['data complete']):
+            pyperclip.copy(data['bar MC'])
+            time.sleep(0.1)
+            keyboard.press_and_release('Ctrl + v, right')
+            time.sleep(0.1)
+            pyperclip.copy(data['LN'])
+            time.sleep(0.1)
+            keyboard.press_and_release('Ctrl + v, right')
+            time.sleep(0.1)
+            pyperclip.copy(data['bar QTY'])
+            time.sleep(0.1)
+            keyboard.press_and_release('Ctrl + v, \n, Home')
 
-                data['data complete'] = False, False
+            data['data complete'] = False, False
 
-                data['MC'] = ''
-                data['LN'] = ''
-                data['QTY'] = ''
+            data['MC'] = ''
+            data['LN'] = ''
+            data['QTY'] = ''
 
-                data['barcode'] = ''
-                data['bar MC'] = ''
-                data['bar QTY'] = ''
+            data['barcode'] = ''
+            data['bar MC'] = ''
+            data['bar QTY'] = ''
 
-                data['date'] = ''
-                data['lot'] = ''
+            data['date'] = ''
+            data['lot'] = ''
 
 
 def show(data):
@@ -128,6 +139,8 @@ def show(data):
     while True:
         s, img = data['cap']
         if s:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
             cv2.line(img, (0, 100), (1000, 100), (255, 0, 0), 1)
             cv2.line(img, (0, 200), (1000, 200), (255, 0, 0), 1)
             cv2.line(img, (0, 300), (1000, 300), (255, 0, 0), 1)
@@ -140,22 +153,19 @@ def show(data):
                 putTextRect(img, f"data {data['data complete']}",
                             (10, 20 + 20 * 1), 1, 1, (0, 255, 0), 1)
 
-            putTextRect(img, data['MC'],
-                        (10, 70 + 20 * 0), 1, 1, (0, 255, 0), 1)
-            putTextRect(img, data['QTY'],
-                        (10, 70 + 20 * 1), 1, 1, (0, 255, 0), 1)
             putTextRect(img, data['LN'],
-                        (10, 70 + 20 * 2), 1, 1, (0, 255, 0), 1)
-            putTextRect(img, f"{data['date']}",
-                        (10, 70 + 20 * 4), 1, 1, (0, 255, 0), 1)
-            putTextRect(img, f"{data['lot']}",
-                        (10, 70 + 20 * 5), 1, 1, (0, 255, 0), 1)
+                        (10, 70 + 20 * 6), 1, 1, (255, 100, 50), 1)
+            putTextRect(img, f"lot: {data['lot']}",
+                        (10, 70 + 20 * 7), 1, 1, (30, 255, 20), 1)
+            putTextRect(img, f"date: {data['date']}",
+                        (10, 70 + 20 * 8), 1, 1, (30, 255, 20), 1)
+
             putTextRect(img, data['barcode'],
-                        (10, 70 + 20 * 7), 1, 1, (0, 255, 0), 1)
-            putTextRect(img, data['bar MC'],
-                        (10, 70 + 20 * 8), 1, 1, (0, 255, 0), 1)
-            putTextRect(img, data['bar QTY'],
-                        (10, 70 + 20 * 9), 1, 1, (0, 255, 0), 1)
+                        (10, 70 + 20 * 2), 1, 1, (255, 100, 50), 1)
+            putTextRect(img, f"MC: {data['bar MC']}",
+                        (10, 70 + 20 * 3), 1, 1, (30, 255, 20), 1)
+            putTextRect(img, f"QTY: {data['bar QTY']}",
+                        (10, 70 + 20 * 4), 1, 1, (30, 255, 20), 1)
 
             cv2.imshow('frame', img)
             key = cv2.waitKey(1)
--- image_to_text/read_barcode.py
+++ image_to_text/read_barcode.py
@@ -5,8 +5,8 @@ import cv2
 def read_barcodes(image):
     decoded_objects = decode(image)
     for obj in decoded_objects:
-        print("Type:", obj.type)
-        print("Data:", obj.data.decode('utf-8'))
+        # print("Type:", obj.type)
+        # print("Data:", obj.data.decode('utf-8'))
         return obj.data.decode('utf-8')
 
 if __name__ == '__main__':
