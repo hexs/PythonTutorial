484ed0c45cdef0b942640698e2ac52b6af0b36b5
jj <zxjq97@gmail.com>
2024-03-22 09:23:00 +0700

    modify

--- 4. mediapipe/1. Hand Tracking Module.py
+++ 4. mediapipe/1. Hand Tracking Module.py
@@ -1,25 +1,58 @@
+import random
+
+import cvzone
 from cvzone.HandTrackingModule import HandDetector
+import json
 import cv2
+from Rect import Point, Rect
 
-# Initialize the webcam to capture video
-# The '0' indicates the third camera connected to your computer; '0' would usually refer to the built-in camera
+pos = Point(0, 0)
+start_pos = Point(0, 0)
+stop_pos = Point(0, 0)
+dwg = False
+hand_have = ''
+parts = {'p1': '-', 'p2': '-', 'p3': '-'}
+
+def click_event(event, x, y, flags, params):
+    global pos, start_pos, stop_pos, dwg
+    # pos = Point(x, y)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        start_pos = Point(x, y)
+        stop_pos = Point(x, y)
+        dwg = True
+    if event == cv2.EVENT_LBUTTONUP:
+        stop_pos = Point(x, y)
+        dwg = False
+    if event == cv2.EVENT_MOUSEMOVE and dwg == True:
+        stop_pos = Point(x, y)
+
+
+rects = {}
+try:
+    with open("datasave.json", "r") as f:  # Read
+        string = f.read()
+    dct = json.loads(string)
+    for k, v in dct.items():
+        name = k
+        x1, y1, x2, y2 = v
+        rects[name] = Rect(name, Point(x1, y1), Point(x2, y2))
+except:
+    pass
+
 cap = cv2.VideoCapture(0)
-
-# Initialize the HandDetector class with the given parameters
 detector = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
-
-# Continuously get frames from the webcam
 while True:
-    # Capture each frame from the webcam
-    # 'success' will be True if the frame is successfully captured, 'img' will contain the frame
     success, img = cap.read()
+    img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5)
 
-    # Find hands in the current frame
-    # The 'draw' parameter draws landmarks and hand outlines on the image if set to True
-    # The 'flipType' parameter flips the image, making it easier for some detections
+    r1 = Rect('-', start_pos, stop_pos)
+    for rect in rects.values():
+        rect.show(img, pos)
+    r1.show(img, pos)
+
     hands, img = detector.findHands(img, draw=True, flipType=True)
 
-    # Check if any hands are detected
+    hand_right = None
     if hands:
         # Information for the first hand detected
         hand1 = hands[0]  # Get the first hand detected
@@ -28,15 +61,9 @@ while True:
         center1 = hand1['center']  # Center coordinates of the first hand
         handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
 
-        # Count the number of fingers up for the first hand
-        fingers1 = detector.fingersUp(hand1)
-        print(f'H1 = {fingers1.count(1)}', end=" ")  # Print the count of fingers that are up
+        if handType1 == "Right":
+            hand_right = lmList1
 
-        # Calculate distance between specific landmarks on the first hand and draw it on the image
-        length, info, img = detector.findDistance(lmList1[8][0:2], lmList1[12][0:2], img, color=(255, 0, 255),
-                                                  scale=10)
-
-        # Check if a second hand is detected
         if len(hands) == 2:
             # Information for the second hand
             hand2 = hands[1]
@@ -45,18 +72,52 @@ while True:
             center2 = hand2['center']
             handType2 = hand2["type"]
 
-            # Count the number of fingers up for the second hand
-            fingers2 = detector.fingersUp(hand2)
-            print(f'H2 = {fingers2.count(1)}', end=" ")
+            if handType2 == "Right":
+                hand_right = lmList1
 
-            # Calculate distance between the index fingers of both hands and draw it on the image
-            length, info, img = detector.findDistance(lmList1[8][0:2], lmList2[8][0:2], img, color=(255, 0, 0),
-                                                      scale=10)
+    if hand_right:
+        x, y, z = hand_right[12]
+        pos = Point(x, y)
+    else:
+        pos = Point(0, 0)
 
-        print(" ")  # New line for better readability of the printed output
 
-    # Display the image in a window
+    for k, v in rects.items():
+        if v.has_point_on_it(pos):
+            if k != 'main':
+                hand_have = k
+
+    if hand_have:
+        if rects['main'].has_point_on_it(pos):
+            parts[hand_have] = 'ok'
+            hand_have = ''
+
+
+
+    cvzone.putTextRect(img, f'hand have {hand_have}', (10, 30), 2, 2, (0, 0, 0), (255, 255, 255), offset=8)
+    for i, (k, v) in enumerate(parts.items()):
+        cvzone.putTextRect(img, f'{k} {v}', (10, 65 + 35 * i), 2, 2, (0, 0, 0), (255, 255, 255), offset=8)
+
+
     cv2.imshow("Image", img)
+    cv2.setMouseCallback('Image', click_event)
+    key = cv2.waitKey(1)
+    if key == -1:
+        continue
+    print(key)
+    if key == ord("a"):
+        name = ''
+        for i in range(5):
+            name += random.choice('abcdefghijklmnopqrqtuvwxyz')
+        r1.name = name
+        rects[name] = r1
 
-    # Keep the window open and update it for each frame; wait for 1 millisecond between frames
-    cv2.waitKey(1)
+        lts = {}
+        for rect in rects.values():
+            lts[rect.name] = rect.xyxy()
+        string = json.dumps(lts, indent=2)
+        with open("datasave.json", "w") as fy:  # Write
+            fy.write(string)
+
+    if key == ord('r'):
+        parts = {'p1': '-', 'p2': '-', 'p3': '-'}
--- 4. mediapipe/Rect.py
+++ 4. mediapipe/Rect.py
@@ -1,4 +1,5 @@
 import cv2
+import cvzone
 
 BLACK = '\033[90m'
 RED = '\033[91m'
@@ -18,33 +19,20 @@ class Point:
         self.x = x
         self.y = y
         self.xy = x, y
-        self.index_iteration = 0
 
     def __str__(self):
         return f'{BLUE}({self.x} {self.y}){ENDC}'
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        self.index_iteration += 1
-        if self.index_iteration == 1:
-            return self.x
-        elif self.index_iteration == 2:
-            return self.y
-        else:
-            self.index_iteration = 0
-            raise StopIteration
-
 
 class Rect:
-    def __init__(self, p1: Point, p2: Point):
+    def __init__(self, name, p1: Point, p2: Point):
+        self.name = name
         self.p1 = p1
         self.p2 = p2
         self.xywh()
 
     def __str__(self):
-        return f'Rect({self.p1} {self.p2})'
+        return f'Rect|{self.name} {self.p1}{self.p2}'
 
     def has_point_on_it(self, p: Point):
         return self.x < p.x < self.x + self.w and self.y < p.y < self.y + self.h
@@ -63,8 +51,13 @@ class Rect:
         self.y2 = max(self.p1.y, self.p2.y)
         return self.x1, self.y1, self.x2, self.y2
 
-    def show(self, img, color):
-        cv2.rectangle(img, self.p1.xy, self.p2.xy, color, 2)  # กรอบน้ำเงิน
+    def show(self, img, pos):
+        cv2.rectangle(img, list(self.p1.xy), list(self.p2.xy), (255, 0, 0), 2)
+        if self.has_point_on_it(pos):
+            cv2.rectangle(img, list(self.p1.xy), list(self.p2.xy), (0, 255, 0), 2)
+        self.xyxy()
+        cvzone.putTextRect(img, self.name, (self.x1, self.y1), 1, 1,
+                           (255, 255, 0), (0, 0, 0), offset=2)
 
 
 if __name__ == '__main__':
@@ -72,5 +65,5 @@ if __name__ == '__main__':
     p2 = Point(300, 400)
     print(p1)
     print(p2)
-    r1 = Rect(p1, p2)
+    r1 = Rect('name', p1, p2)
     print(r1)
